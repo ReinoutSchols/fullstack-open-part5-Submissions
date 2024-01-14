1470a3acadf2f1ce918467e9d72e6e6634683295
Reinout Schols <reinoutschols@hotmail.com>
2024-01-14 16:55:28 +0100

    Completed exercise 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,7 +38,20 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [user])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
   
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -69,7 +87,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in </p>
+      <p>{user.username} logged in </p> 
+      <button onClick={() => handleLogout()}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
